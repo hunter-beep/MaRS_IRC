4448af2657f954d78824d0923d7880c27baa7c96
Sarang_R <65761098+hunter-beep@users.noreply.github.com>
2025-01-02 01:11:08 +0530

    Update FINAL_MANIPULATOR.cpp

--- FINAL_MANIPULATOR.cpp
+++ FINAL_MANIPULATOR.cpp
@@ -1,31 +1,12 @@
 #include <Arduino.h>
-#include <ros.h>
-#include <std_msgs/Int16.h>
 
 
-#define LA1dir PC15
-#define LA1pwm PA0
-#define LA2dir PA1
-#define LA2pwm PA2
-#define M1dir PA5
-#define M1pwm PA6
-#define basedir PB15
-#define basepwm PB14
-#define beveldir
-#define bevelpwm
-#define gripperdir
-#define gripperpwm
-
-
-int pwm=0; //pwm global has to be changed
-
-
-class LA {
+class LA
+{
   private:
     int pwm_pin;
     int dir_pin;
-
-
+ 
   public:
     LA(int, int);
     void assign_pwm(int);
@@ -35,43 +16,47 @@ class LA {
 };
 
 
-LA::LA(int DIR_PIN, int PWM_PIN) {
+LA::LA(int DIR_PIN, int PWM_PIN)
+{
   dir_pin = DIR_PIN;
   pwm_pin = PWM_PIN;
-  pinMode(dir_pin, OUTPUT);
-  pinMode(pwm_pin, OUTPUT);
-  digitalWrite(dir_pin, LOW);
-  analogWrite(pwm_pin, 0);
+  pinMode(dir_pin,OUTPUT);
+  pinMode(pwm_pin,OUTPUT);
+  digitalWrite(dir_pin,LOW);
+  analogWrite(pwm_pin,0);
 }
 
 
-void LA::assign_pwm(int pwm) {
-  analogWrite(pwm_pin, pwm);
+void LA::assign_pwm(int pwm)
+{
+  analogWrite(pwm_pin,pwm);
 }
 
 
-void LA::forward() {
-  digitalWrite(dir_pin, HIGH);
+void LA::forward()
+{
+  digitalWrite(dir_pin,HIGH);
 }
 
 
-void LA::backward() {
-  digitalWrite(dir_pin, LOW);
+void LA::backward()
+{
+  digitalWrite(dir_pin,LOW);
 }
 
 
-void LA::stop() {
-  digitalWrite(dir_pin, LOW);
-  analogWrite(pwm_pin, 0);
+void LA::stop()
+{
+  digitalWrite(dir_pin,LOW);
+  analogWrite(pwm_pin,0);
 }
-
-
-class motor {
+///////////////////////////////////////////////////////////
+class motor
+{
   private:
     int pwm_pin;
     int dir_pin;
-
-
+ 
   public:
     motor(int, int);
     void assign_pwm(int);
@@ -81,161 +66,130 @@ class motor {
 };
 
 
-motor::motor(int DIR_PIN, int PWM_PIN) {
+motor::motor(int DIR_PIN, int PWM_PIN)
+{
   dir_pin = DIR_PIN;
   pwm_pin = PWM_PIN;
-  pinMode(dir_pin, OUTPUT);
-  pinMode(pwm_pin, OUTPUT);
-  digitalWrite(dir_pin, LOW);
-  analogWrite(pwm_pin, 0);
+  pinMode(dir_pin,OUTPUT);
+  pinMode(pwm_pin,OUTPUT);
+  digitalWrite(dir_pin,LOW);
+  analogWrite(pwm_pin,0);
 }
 
 
-void motor::assign_pwm(int pwm) {
-  analogWrite(pwm_pin, pwm);
+void motor::assign_pwm(int pwm)
+{
+  analogWrite(pwm_pin,pwm);
 }
 
 
-void motor::CW() { digitalWrite(dir_pin, HIGH); }
-
-
-void motor::ACW() { digitalWrite(dir_pin, LOW); }
-
-
-void motor::stop() {
-  digitalWrite(dir_pin, LOW);
-  analogWrite(pwm_pin, 0);
+void motor::CW()    //clockwise
+{
+  digitalWrite(dir_pin,HIGH);
 }
 
 
+void motor::ACW()   //anticlockwise
+{
+  digitalWrite(dir_pin,LOW);
+}
 
 
-LA link1(LA1dir, LA1pwm);
-LA link2(LA2dir, LA2pwm);
-motor link3(M1dir, M1pwm);
-motor base(basedir, basepwm);
-motor bevel_gear(beveldir, bevelpwm);
-motor gripper(gripperdir, gripperpwm);
+void motor::stop()
+{
+  digitalWrite(dir_pin,LOW);
+  analogWrite(pwm_pin,0);
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-ros::NodeHandle nh;
-std_msgs::Int16 pwm_msg;
-std_msgs::Int16 choice_msg;
 
 
-// ros subscriber callbacks
-void choiceCallback(const std_msgs::Int16 &msg) {
-  int choice = msg.data;
+LA link1(PC15, PA0); //FLW1
+LA link2(PA1, PA2); //MLW1
+motor link3(PA5,PA6); //BLW1
+motor base(PB15,PB14); //BRW1
+
+
+int choice;
+int pwm; //common pwm for all links. if needed, make one pwm for actuators and one for motors later
+
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+
+void loop()
+{
+  Serial.println("choice:");
+  while (Serial.available() == 0);
+  choice = Serial.parseInt();
+
+
  
 
 
-  switch (choice) {                              
-    case 1:
-      link1.assign_pwm(pwm);
-      link1.forward();
-      break;
+  switch(choice)
+  {
+    case 1: //knob 1 forward
+      Serial.println("pwm:");
+      while (Serial.available() == 0);
+      pwm = Serial.parseInt();
+      link1.assign_pwm(pwm); link1.forward(); break;
 
 
-    case 2:
-      link1.assign_pwm(pwm);
-      link1.backward();
-      break;
+    case 2:  //knob 1 backward
+      Serial.println("pwm:");
+      while (Serial.available() == 0);
+      pwm = Serial.parseInt();
+      link1.assign_pwm(pwm); link1.backward(); break;
 
 
-    case 3:
-      link2.assign_pwm(pwm);
-      link2.forward();
-      break;
+    case 3: //knob 2 forward
+      Serial.println("pwm:");
+      while (Serial.available() == 0);
+      pwm = Serial.parseInt();
+      link2.assign_pwm(pwm); link2.forward(); break;
 
 
-    case 4:
-      link2.assign_pwm(pwm);
-      link2.backward();
-      break;
+    case 4: //knob 2 backward
+      Serial.println("pwm:");
+      while (Serial.available() == 0);
+      pwm = Serial.parseInt();
+      link2.assign_pwm(pwm); link2.backward(); break;
 
 
-    case 5:
-      link3.assign_pwm(pwm);
-      link3.CW();
-      break;
+    case 5: //knob 1 right
+      Serial.println("pwm:");
+      while (Serial.available() == 0);
+      pwm = Serial.parseInt();
+      link3.assign_pwm(pwm); link3.CW(); break;  
 
 
-    case 6:
-      link3.assign_pwm(pwm);
-      link3.ACW();
-      break;
+    case 6: //knob 1 left
+      Serial.println("pwm:");
+      while (Serial.available() == 0);
+      pwm = Serial.parseInt();
+      link3.assign_pwm(pwm); link3.ACW(); break;
 
 
-    case 7:
-      base.assign_pwm(pwm);
-      base.CW();
-      break;
+    case 7: //knob 2 right
+      Serial.println("pwm:");
+      while (Serial.available() == 0);
+      pwm = Serial.parseInt();
+      base.assign_pwm(pwm); base.CW(); break;
 
 
-    case 8:
-      base.assign_pwm(pwm);
-      base.ACW();
-      break;
-
-
-    case 9:
-      bevel_gear.assign_pwm(pwm);
-      bevel_gear.CW();
-      break;
-
-
-    case 10:
-      bevel_gear.assign_pwm(pwm);
-      bevel_gear.ACW();
-      break;
-
-
-    case 11:
-      gripper.assign_pwm(pwm);
-      gripper.CW();
-      break;
-
-
-    case 12:
-      gripper.assign_pwm(pwm);
-      gripper.ACW();
-      break;
-   
+    case 8: //knob 2 left
+      Serial.println("pwm:");
+      while (Serial.available() == 0);
+      pwm = Serial.parseInt();
+      base.assign_pwm(pwm); base.ACW(); break;
 
 
     default:
-      link1.stop();
-      link2.stop();
-      link3.stop();
-      base.stop();
-	gripper.stop();
-	bevel_gear.stop();
-      break;
+       link1.stop(); link2.stop(); link3.stop(); base.stop(); break;
   }
-}
-
-
-void pwmCallback(const std_msgs::Int16 &msg) {
-  pwm = msg.data;
- 
-}
-
-
-
-
-ros::Subscriber<std_msgs::Int16> pwm_sub("/manipulator/pwm", &pwmCallback);
-ros::Subscriber<std_msgs::Int16> choice_sub("/manipulator/choice", &choiceCallback);
-
-
-void setup() {
-  nh.initNode();
-  nh.subscribe(choice_sub);
-  nh.subscribe(pwm_sub);
-  nh.loginfo("Arduino Node Initialized");
-}
-
-
-void loop() {
-  nh.spinOnce();
-  delay(10);
 }
